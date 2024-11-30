b109bad07e9b6e4dd221b947e8d1bb06d24c0cbb
Zaid <zaidalqadiri@gmail.com>
2024-11-29 20:03:07 -0700

    implemented styling

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -36,7 +36,7 @@ function Stopwatch() {
             <div className="display">{formatTime()}</div>
             <div className="controls">
                 <button onClick={start} className="start-button">Start</button>
-                <button onClick={start} className="start-button">Start</button>
+                <button onClick={stop} className="stop-button">Stop</button>
                 <button onClick={reset} className="reset-button">Reset</button>
             </div>
 
